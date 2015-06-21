597e9f75587989f36d6e7c932374ca01f6b8e02d
AarArber <Aaron.Arber15@houghton.edu>
2015-06-20 20:11:00 -0400

    Initial Commit
    Committing entire new Cram project

---
+++ android/src/com/totallyserious/cram/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.totallyserious.cram.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.totallyserious.cram.MyCram;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyCram(), config);
+	}
+}
