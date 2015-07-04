d187f48e1bb7c133594f456c365fb9050ac81b6c
AarArber <Aaron.Arber15@houghton.edu>
2015-07-03 21:48:27 -0400

    Adding description, among other files.

---
+++ core/src/com/totallyserious/cram/screens/MainMenu.java
@@ -0,0 +1,12 @@
+package com.totallyserious.cram.screens;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Aaron on 7/1/2015.
+ */
+public class MainMenu {
+    private Stage stage = new Stage();
+
+
+}
---
+++ core/src/com/totallyserious/cram/screens/Splash.java
@@ -0,0 +1,38 @@
+package com.totallyserious.cram.screens;
+
+/**
+ * Created by Aaron on 7/1/2015.
+ */
+import com.badlogic.gdx.Screen;
+
+public class Splash implements Screen {
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+}
