4fde3a34a07d9ec8a2ded1f4d2d6122264e8622d
AarArber <Aaron.Arber15@houghton.edu>
2015-07-09 00:02:21 -0400

    This class controls the ceiling and floor 'Walls'

---
+++ core/src/com/totallyserious/cram/Crammer.java
@@ -0,0 +1,18 @@
+package com.totallyserious.cram;
+
+/**
+ * Created by Aaron on 7/8/2015.
+ */
+public class Crammer {
+    private Wall ceiling;
+    private Wall floor;
+
+    Crammer(){
+        ceiling.setDirection(1);
+        floor.setDirection(-1);
+    }
+
+    public void move(){
+        
+    }
+}
